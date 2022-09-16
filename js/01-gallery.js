535e43a703c2926c1ec7dd21ca6318f237511a6b
Roman <roman.baytala@gmail.com>
2022-09-16 15:46:57 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const IMG_NODE = "IMG";
+const ESC_KEY = "Escape";
+const galleryRef = document.querySelector('.gallery');
+const cardCreateEvt = cardCreate(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', cardCreateEvt);
+galleryRef.addEventListener('click', ModalOpen);
 
-console.log(galleryItems);
+function cardCreate(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`
+    }).join('');
+}
+
+function ModalOpen(evt) {
+    evt.preventDefault();
+    const { nodeName, dataset } = evt.target;
+    nodeName === IMG_NODE && showModal(dataset.source);
+}
+
+function showModal(source) {
+    const handleModalClose = ({ code }) => {
+        code === ESC_KEY && modal.close();
+    };
+    const modalMarkup = `<img src="${source}">`;
+    const modalOptions = {
+        onShow: () => {
+            document.addEventListener("keydown", handleModalClose);
+        },
+        onClose: () => {
+            document.removeEventListener("keydown", handleModalClose);
+        },
+    };
+    const modal = basicLightbox.create(modalMarkup, modalOptions).show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const cardCreateEvt = cardCreate(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', cardCreateEvt);
 
-console.log(galleryItems);
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+
+function cardCreate(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join(''); 
+}
+
