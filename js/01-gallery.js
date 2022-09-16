f6b5902e00f795acc23dc375aeea1ddc47268fa8
Roman <roman.baytala@gmail.com>
2022-09-16 15:55:02 +0300

    hw-7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,6 +6,28 @@ const cardCreateEvt = cardCreate(galleryItems);
 galleryRef.insertAdjacentHTML('beforeend', cardCreateEvt);
 galleryRef.addEventListener('click', ModalOpen);
 
+function ModalOpen(evt) {
+    evt.preventDefault();
+    const { nodeName, dataset } = evt.target;
+    nodeName === IMG_NODE && showModal(dataset.source);
+}
+
+function showModal(src) {
+    const handleModalClose = ({ code }) => {
+        code === ESC_KEY && modal.close();
+    };
+    const modalMarkup = `<img src="${src}">`;
+    const modalOptions = {
+        onShow: () => {
+            document.addEventListener("keydown", handleModalClose);
+        },
+        onClose: () => {
+            document.removeEventListener("keydown", handleModalClose);
+        },
+    };
+    const modal = basicLightbox.create(modalMarkup, modalOptions)
+    modal.show();
+}
 function cardCreate(galleryItems){
     return galleryItems
     .map(({preview, original, description}) => {
@@ -20,26 +42,4 @@ function cardCreate(galleryItems){
             </a>
         </div>`
     }).join('');
-}
-
-function ModalOpen(evt) {
-    evt.preventDefault();
-    const { nodeName, dataset } = evt.target;
-    nodeName === IMG_NODE && showModal(dataset.source);
-}
-
-function showModal(source) {
-    const handleModalClose = ({ code }) => {
-        code === ESC_KEY && modal.close();
-    };
-    const modalMarkup = `<img src="${source}">`;
-    const modalOptions = {
-        onShow: () => {
-            document.addEventListener("keydown", handleModalClose);
-        },
-        onClose: () => {
-            document.removeEventListener("keydown", handleModalClose);
-        },
-    };
-    const modal = basicLightbox.create(modalMarkup, modalOptions).show();
 }
